1f49a1d7e8650ec87311270353e6617079bb740f
lajiduxiu <44544799+lajiduxiu@users.noreply.github.com>
2020-11-24 16:04:24 +0800

    Update Test.java

--- src/main/java/com/dd/Test.java
+++ src/main/java/com/dd/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("我输出");
-
+        System.out.println("我输出12");
         System.out.println("我输出11");
 
     }
