a71deb23aa20a6e28e1ae911396a580b77949a61
lajiduxiu <44544799+lajiduxiu@users.noreply.github.com>
2020-11-24 16:08:49 +0800

    Update Test.java

--- src/main/java/com/dd/Test.java
+++ src/main/java/com/dd/Test.java
@@ -6,6 +6,9 @@ public class Test {
 
     public static void main(String[] args) {
 
+        System.out.println("我是主干代码的输出");
+        System.out.println("我是主干代码的输出1");
+        System.out.println("我是主干代码的输出2");
 
 
     }
